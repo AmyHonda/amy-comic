aeca5d2d38af0502532c1edcfb5b874f7e9984a9
EMIKO HONDA <emikohonda@EMIKOnoMBP.lan>
2025-12-13 17:33:22 +0900

    ブルスカをメニューに追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <nav className={`navbar ${isOpen ? "open" : ""}`}>
             <Link href="/" onClick={() => setIsOpen(false)}>ホーム</Link>
             <Link href="https://note.com/dot_emiko" onClick={() => setIsOpen(false)}>ブログ</Link>
+            <Link href="https://bsky.app/profile/amycomic.bsky.social" onClick={() => setIsOpen(false)}>つぶやき</Link>
             <Link href="/characters" onClick={() => setIsOpen(false)}>登場人物</Link>
             <Link href="/main" onClick={() => setIsOpen(false)}>本編</Link>
             <Link href="/sub" onClick={() => setIsOpen(false)}>番外編</Link>
